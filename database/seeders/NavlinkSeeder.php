e3a814b84f97d280e2eaa7afe080dfcd3434aee5
Piyush Gupta <pg.softcode@gmail.com>
2021-06-25 13:23:44 +0530

    Navbar Redefined

--- database/seeders/NavlinkSeeder.php
+++ database/seeders/NavlinkSeeder.php
@@ -16,38 +16,22 @@ class NavlinkSeeder extends Seeder
     {
         // ID 1
         Navlink::create(['group' => 'navbar',
-            'title' => 'Home', 'slug' => 'home',
+            'title' => 'Event', 'slug' => 'events',
         ]);
 
         // ID 2
-        Navlink::create(['group' => 'navbar', 'hasChild' => true,
-            'title' => 'Event', 'slug' => 'event',
+        Navlink::create(['group' => 'navbar',
+            'title' => 'Guest','slug' => 'guests',
         ]);
 
         // ID 3
-        Navlink::create(['group' => 'navbar',  'hasChild' => true,
-            'title' => 'People','slug' => 'people',
+        Navlink::create(['group' => 'navbar',
+            'title' => 'Tasks', 'slug' => 'tasks',
         ]);
 
         // ID 4
         Navlink::create(['group' => 'navbar',
-            'title' => 'Invitation', 'slug' => 'invitation',
-        ]);
-
-        // Sub-menu for ID 2
-        Navlink::create(['parent_id' => 2, 'group' => 'navbar',
-            'title' => 'Occasion', 'slug' => 'occasion',
-        ]);
-        Navlink::create(['parent_id' => 2, 'group' => 'navbar',
-            'title' => 'Ritual', 'slug' => 'ritual',
-        ]);
-
-        // Sub-menu for ID 3
-        Navlink::create(['parent_id' => 3, 'group' => 'navbar',
-            'title' => 'Guest', 'slug' => 'guest',
-        ]);
-        Navlink::create(['parent_id' => 3, 'group' => 'navbar',
-            'title' => 'Vendor', 'slug' => 'vendor',
+            'title' => 'Profile', 'slug' => 'profile',
         ]);
 
     }
